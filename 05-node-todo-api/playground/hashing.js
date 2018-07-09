651c3cec127c9454045e51d222dc3c28b756db01
Ivaylo Hristov <ishristov@users.noreply.github.com>
2018-07-09 10:55:55 +0300

    todos: hash user password before saving

--- 05-node-todo-api/playground/hashing.js
+++ 05-node-todo-api/playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 10
-}
+var password = '123abc'
 
-var token = jwt.sign(data, '123abc')
-console.log(token)
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log('hash: ', hash)
+//   })
+// })
 
-var decoded = jwt.verify(token, '123abc')
-console.log('decoded', decoded)
+var hashedPassword = '$2a$10$VE0I2a7HsucLRR35fyUgluvu7YtCP/SOtRsanq7J6OMA1/8rr0g3C'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log('res: ', res)
+})
+
+// var data = {
+//   id: 10
+// }
+
+// var token = jwt.sign(data, '123abc')
+// console.log(token)
+
+// var decoded = jwt.verify(token, '123abc')
+// console.log('decoded', decoded)
 // jwt.verify
 
 // var message = 'User 3'
