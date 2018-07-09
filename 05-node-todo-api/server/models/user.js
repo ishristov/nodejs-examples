f6d8322851a2c0b8438b3bc1f7f0cde09534dcc7
Ivaylo Hristov <ishristov@users.noreply.github.com>
2018-07-09 10:40:14 +0300

    todo api: add POST /users and authenticate middleware

--- 05-node-todo-api/server/models/user.js
+++ 05-node-todo-api/server/models/user.js
@@ -1,12 +1,73 @@
+const _ = require('lodash')
 const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
+
+// Return only some user properties
+UserSchema.methods.toJSON = function () {
+  const user = this
+  const userObject = user.toObject()
+
+  return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  const user = this
+  const access = 'auth'
+  const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+
+  user.tokens = user.tokens.concat([{access, token}])
+  return user.save().then(() => {
+    return token
+  })
+}
+
+UserSchema.statics.findByToken = function (token) {
+  const User = this
+  let decoded
+
+  try {
+    decoded = jwt.verify(token, 'abc123')
+  } catch (e) {
+    return Promise.reject()
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+}
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
--- 05-node-todo-api/server/server.js
+++ 05-node-todo-api/server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb')
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
+const {authenticate} = require('./middleware/authenticate')
 
 const app = express()
 const port = process.env.PORT
@@ -93,6 +94,23 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send()
   })
+})
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password'])
+  const user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(port, () => {
