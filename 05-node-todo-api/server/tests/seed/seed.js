0b47d2fa0fd80f23784d16c7b62c3a6bcc13be59
Ivaylo Hristov <ishristov@users.noreply.github.com>
2018-07-10 10:12:08 +0300

    Associate users with todos

--- 05-node-todo-api/server/tests/seed/seed.js
+++ 05-node-todo-api/server/tests/seed/seed.js
@@ -18,16 +18,22 @@ const users = [{
   _id: userTwoId,
   email: 'peter@example.com',
   password: 'secondPassword',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }]
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First'
+  text: 'First',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second',
   completed: true,
-  completedAt: 111
+  completedAt: 111,
+  _creator: userTwoId
 }]
 
 const populateTodos = (done) => {
