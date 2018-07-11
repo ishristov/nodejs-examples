2614a420afd3e081a71bb5751eea35445ec72905
Ivaylo Hristov <ishristov@users.noreply.github.com>
2018-07-11 09:26:30 +0300

    Use config.json for dev/test env

--- 05-node-todo-api/server/config/config.js
+++ 05-node-todo-api/server/config/config.js
@@ -1,11 +1,10 @@
 const env = process.env.NODE_ENV || 'development'
 
-if (env === 'development') {
-  process.env.PORT = 3000
-  process.env.CUSTOM_MONGODB = 'mongodb://localhost:27017/todoApp'
-} else if (env === 'test') {
-  process.env.PORT = 3000
-  process.env.CUSTOM_MONGODB = 'mongodb://localhost:27017/todoAppTest'
-} else {
-  process.env.CUSTOM_MONGODB = 'mongodb://test:test1234@ds161520.mlab.com:61520/todo-app'
+if (env === 'development' || env === 'test') {
+  const config = require('./config.json')
+  const envConfig = config[env]
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key]
+  })
 }
--- 05-node-todo-api/server/tests/seed/seed.js
+++ 05-node-todo-api/server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users = [{
   password: 'firstPassword',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }, {
   _id: userTwoId,
@@ -20,7 +20,7 @@ const users = [{
   password: 'secondPassword',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }]
 
